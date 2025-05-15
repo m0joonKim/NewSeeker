ac6e213cb16a93dbe15f70020f3fcd1fd514c1bb
Myoungjoon Kim <m0joonk@gmail.com>
2025-05-15 15:16:45 +0900

    refact

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -5,5 +5,5 @@ from app.core.config import settings
 api_router = APIRouter()
 
 
-if settings.ENVIRONMENT == "local":
-    api_router.include_router(private.router)
+# if settings.ENVIRONMENT == "local":
+#     api_router.include_router(private.router)
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "your-secret-key-here"
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    FRONTEND_URL: str = "http://localhost:5173"
+    FRONTEND_URL: str = "http://localhost:3000"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     BACKEND_CORS_ORIGINS: Annotated[
